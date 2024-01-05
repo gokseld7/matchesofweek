64d778d64c248b7981bf89d03dfc14b5cb407398
gdarcan <gdarcan@assia-inc.com>
2024-01-05 19:12:34 +0300

    Updates notation, fixes xpath parsing error

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import datetime as dt
 from bs4 import BeautifulSoup
 from lxml import etree
 from typing import Union
-from userconf import favouriteTeams
+from userconf import favourite_teams
 
 # TODO: prepare to get web results all in English and resolve everything in English
 # TODO: after converting English, set the dd/mm or mm/dd format
 # TODO: write simple GUI with pyqt
 # TODO: Dockerize the app to make this project os agnostic
-# TODO: Is there any way to make getNextMatches better? Maybe getNextMatch as an individual function
+# TODO: Is there any way to make get_next_matches better?
+#       Maybe getNextMatch as an individual function
+# TODO: Find a better parsing method from xpath
 
 
-def sendGetRequest(url: str, headers: dict) -> Union[str, None]:
+def get_request(url: str, headers: dict) -> Union[str, None]:
     max_retries = 3
     for i in range(max_retries):
         response = requests.get(url, headers=headers)
@@ -22,7 +24,7 @@ def sendGetRequest(url: str, headers: dict) -> Union[str, None]:
     return None
 
 
-def getNextMatchWebRequest(teamName: str) -> Union[None, etree._Element]:
+def get_next_match_web_request(teamName: str) -> Union[None, etree._Element]:
     teamName = teamName.replace(" ", "+")
     url = f"https://www.google.com/search?q={teamName}+next+match"
     headers = (
@@ -33,7 +35,7 @@ def getNextMatchWebRequest(teamName: str) -> Union[None, etree._Element]:
         }
     )
 
-    response = sendGetRequest(url, headers)
+    response = get_request(url, headers)
     if response is None:
         print(f"Failed request for {teamName}!")
         return None
@@ -42,68 +44,74 @@ def getNextMatchWebRequest(teamName: str) -> Union[None, etree._Element]:
     return etree.HTML(str(soup))
 
 
-def getXPaths(data: etree._Element) -> dict:
-    xPaths = dict()
+def get_xpaths(data: etree._Element) -> dict:
+    xpaths = dict()
     index = 3
-    commonPath = f"//*[@id='sports-app']/div/div[3]/div[1]/div[{index}]/div/div/div/div[1]/div"
+    common_path = f"//*[@id='sports-app']/div/div[3]/div[1]/div[{index}]/div/div/div/div[1]/div"
 
-    homeTeam = f"{commonPath}/div[2]/div[1]/div/div[1]/div[2]/div/span"
-    if not data.xpath(homeTeam):
+    if not data.xpath(common_path):
+        common_path = f"//*[@id='sports-app']/div/div[3]/div[1]/div[{index}]/div/div/div/div[2]/div"
+
+    home_team = f"{common_path}/div[2]/div[1]/div/div[1]/div[2]/div/span"
+    if not data.xpath(home_team):
         index = 4
-        commonPath = f"//*[@id='sports-app']/div/div[3]/div[1]/div[{index}]/div/div/div/div[1]/div"
+        common_path = f"//*[@id='sports-app']/div/div[3]/div[1]/div[{index}]/div/div/div/div[1]/div"
 
-    xPaths["homeTeam"] = f"{commonPath}/div[2]/div[1]/div/div[1]/div[2]/div/span"
-    xPaths["awayTeam"] = f"{commonPath}/div[2]/div[1]/div/div[3]/div[2]/div/span"
-    xPaths["leagueName"] = f"{commonPath}/div[1]/div/span[1]/span[1]/span"
-    xPaths["dateTime"] = f"{commonPath}/div[1]/div/span[2]"
-    return xPaths
+    xpaths["homeTeam"] = f"{common_path}/div[2]/div[1]/div/div[1]/div[2]/div/span"
+
+    xpaths["awayTeam"] = f"{common_path}/div[2]/div[1]/div/div[3]/div[2]/div/span"
+    xpaths["leagueName"] = f"{common_path}/div[1]/div/span[1]/span[1]/span"
+    xpaths["dateTime"] = f"{common_path}/div[1]/div/span[2]"
+    return xpaths
 
 
-def parseDateTime(dateTimeStr: str) -> dt.datetime:
-    dateTimeStr = dateTimeStr.lower()
-    hour, minute = dateTimeStr.split(',')[1].strip().split(':')
+def parse_date_time(date_time_str: str) -> dt.datetime:
+    date_time_str = date_time_str.lower()
+    hour, minute = date_time_str.split(',')[1].strip().split(':')
     hour = int(hour)
     minute = int(minute)
 
-    if '/' in dateTimeStr:
-        day, month = dateTimeStr.split()[0].split('/')
+    if '/' in date_time_str:
+        day, month = date_time_str.split()[0].split('/')
         day = int(day)
         month = int(month)
-        dateTime = dt.datetime(dt.datetime.now().year, month, day, hour, minute, 0, 0)
-        return dateTime
+        date_time = dt.datetime(dt.datetime.now().year, month, day, hour, minute, 0, 0)
+        return date_time
 
-    dateTime = dt.datetime.now()
-    if "yarın" in dateTimeStr or "tomorrow" in dateTimeStr:
-        dateTime = dateTime + dt.timedelta(days=1)
+    date_time = dt.datetime.now()
+    if "yarın" in date_time_str or "tomorrow" in date_time_str:
+        date_time = date_time + dt.timedelta(days=1)
 
-    dateTime = dateTime.replace(hour=hour)
-    dateTime = dateTime.replace(minute=minute)
-    dateTime = dateTime.replace(second=0).replace(microsecond=0)
+    date_time = date_time.replace(hour=hour)
+    date_time = date_time.replace(minute=minute)
+    date_time = date_time.replace(second=0).replace(microsecond=0)
 
-    return dateTime
+    return date_time
 
 
-def getNextMatches(teamName: str, matches: list, lock: threading.Lock) -> None:
-    data = getNextMatchWebRequest(teamName)
+def get_next_matches(team_name: str, matches: list, lock: threading.Lock) -> None:
+    data = get_next_match_web_request(team_name)
     if data is not None:
-        xPaths = getXPaths(data)
+        xpaths = get_xpaths(data)
         lock.acquire()
         try:
-            dateTime = parseDateTime(f"{data.xpath(xPaths['dateTime'])[0].text}")
+            date_time = parse_date_time(f"{data.xpath(xpaths['dateTime'])[0].text}")
             matches.append(
                 {
-                    "match": f"{data.xpath(xPaths['homeTeam'])[0].text} - {data.xpath(xPaths['awayTeam'])[0].text}",
-                    "league": f"{data.xpath(xPaths['leagueName'])[0].text}",
-                    "dateTime": dateTime
+                    "match":
+                        f"{data.xpath(xpaths['homeTeam'])[0].text} - {data.xpath(xpaths['awayTeam'])[0].text}",
+                    "league":
+                        f"{data.xpath(xpaths['leagueName'])[0].text}",
+                    "dateTime": date_time
                 }
             )
         except IndexError:
-            print(f"Error parsing results for {teamName}")
+            print(f"Error parsing results for {team_name}")
         finally:
             lock.release()
 
 
-def showAllMatches(matches: list) -> None:
+def show_all_matches(matches: list) -> None:
     matches = sorted(matches, key=lambda x: x["dateTime"])
     for match in matches:
         print(20*"-")
@@ -114,16 +122,16 @@ def showAllMatches(matches: list) -> None:
 
 
 def main():
-    if type(favouriteTeams) != set:
+    if type(favourite_teams) != set:
         print("Favourite teams in userconf.py is not a set, please edit it and try again.")
-        print("Example userconf.py: favouriteTeams =  {'Fenerbahce', 'PSV'}")
+        print("Example userconf.py: favourite_teams =  {'Fenerbahce', 'PSV'}")
         return
     matches = []
     threads = []
     lock = threading.Lock()
 
-    for teamName in favouriteTeams:
-        threads.append(threading.Thread(target=getNextMatches, args=(teamName, matches, lock)))
+    for team_name in favourite_teams:
+        threads.append(threading.Thread(target=get_next_matches, args=(team_name, matches, lock)))
 
     for thread in threads:
         thread.start()
@@ -131,7 +139,7 @@ def main():
     for thread in threads:
         thread.join()
 
-    showAllMatches(matches)
+    show_all_matches(matches)
 
 
 if __name__ == '__main__':
--- userconf.py
+++ userconf.py
@@ -1,4 +1,4 @@
 # User configuration
 # You can edit your favourite teams from here
 
-favouriteTeams =  {"Fenerbahce", "PSV", "Borussia Dortmund", "Manchester City", "Real Madrid", "Leipzig"}
+favourite_teams =  {"Fenerbahce", "PSV", "Borussia Dortmund", "Manchester City", "Real Madrid", "Leipzig"}
